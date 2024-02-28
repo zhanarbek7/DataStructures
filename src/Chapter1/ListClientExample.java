e02cf8bacc158d72c77b493a6a1dd6400a5f6f82
Zhanarbek Abdurasulov <zhanarbek.a7@gmail.com>
2024-02-27 18:09:10 -0600

    List interface created.

---
+++ src/Chapter1/ListClientExample.java
@@ -0,0 +1,22 @@
+package Chapter1;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListClientExample {
+    private List list;
+
+    public ListClientExample(){
+        list = new LinkedList();
+    }
+
+    private List getList(){
+        return list;
+    }
+
+    public static void main(String[] args){
+        ListClientExample lce = new ListClientExample();
+        List list = lce.getList();
+        System.out.println(list);
+    }
+}
